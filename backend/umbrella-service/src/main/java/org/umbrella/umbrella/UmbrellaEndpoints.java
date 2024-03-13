bade8fde0f02617fd55401504df52fa60008bf1f
Aleksandr Seppenen <alex.seppenen@gmail.com>
2024-03-13 21:39:21 +0200

    Created basic services create, update, delete, find all

--- backend/umbrella-service/src/main/java/org/umbrella/umbrella/UmbrellaEndpoints.java
+++ backend/umbrella-service/src/main/java/org/umbrella/umbrella/UmbrellaEndpoints.java
@@ -1,13 +1,11 @@
 package org.umbrella.umbrella;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.umbrella.umbrella.dto.EntrepreneurDto;
 import org.umbrella.umbrella.dto.UserResponseDto;
@@ -27,9 +25,9 @@ public class UmbrellaEndpoints {
     }
 
     /**
-     * Retrieves the list of users from the User Service.
+     * Retrieves the list of users from the UserService.
      *
-     * @return the ResponseEntity with the list of users as UserResponseDto objects
+     * @return ResponseEntity<List < UserResponseDto>> - the list of users as a ResponseEntity containing a List of UserResponseDto objects
      */
     @GetMapping("/getAllUsers")
     public ResponseEntity<List<UserResponseDto>> fetchUsersFromUserService() {
@@ -37,30 +35,58 @@ public class UmbrellaEndpoints {
         return ResponseEntity.ok(userServiceUsers);
     }
 
-    @DeleteMapping("/removeEntrepreneurById/{id}")
+    /**
+     * Removes an entrepreneur by their ID.
+     *
+     * @param id the ID of the entrepreneur to be removed
+     * @return a ResponseEntity with no content
+     */
+    @DeleteMapping("/entrepreneurs/{id}")
     public ResponseEntity removeEntrepreneurByID(@PathVariable Long id) {
         entrepreneurService.deleteEntrepreneur(id);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/getEntrepreneurById")
-    public ResponseEntity<EntrepreneurDto> getEntrepreneur(@RequestParam Long id) {
+    /**
+     * Retrieves an EntrepreneurDto object by ID.
+     *
+     * @param id the ID of the entrepreneur to retrieve
+     * @return a ResponseEntity containing the retrieved EntrepreneurDto object
+     */
+    @GetMapping("/entrepreneurs/{id}")
+    public ResponseEntity<EntrepreneurDto> getEntrepreneur(@PathVariable Long id) {
         var result = entrepreneurService.getEntrepreneur(id);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/getAllEntrepreneurs")
+    /**
+     * Retrieves a list of EntrepreneurDto objects.
+     *
+     * @return ResponseEntity<List < EntrepreneurDto>> - the list of entrepreneurs as a ResponseEntity containing a List of EntrepreneurDto objects
+     */
+    @GetMapping("/entrepreneurs")
     public ResponseEntity<List<EntrepreneurDto>> getEntrepreneurs() {
         var result = entrepreneurService.getEntrepreneurs();
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return ResponseEntity.ok(result);
     }
 
-    @PostMapping("/createEntrepreneur")
+    /**
+     * Creates a new entrepreneur.
+     *
+     * @param entrepreneurDto the EntrepreneurDto object representing the entrepreneur to be created
+     * @return a ResponseEntity containing the created EntrepreneurDto object
+     */
+    @PostMapping("/entrepreneurs")
     public ResponseEntity<EntrepreneurDto> createEntrepreneur(@RequestBody EntrepreneurDto entrepreneurDto) {
         var result = entrepreneurService.createEntrepreneur(entrepreneurDto);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return ResponseEntity.ok(result);
     }
 
+    /**
+     * Retrieves the health status of the application.
+     *
+     * @return ResponseEntity - the response entity indicating the health status of the application
+     */
     @GetMapping("/health")
     public ResponseEntity getHealth() {
         return ResponseEntity.ok("Application is running");
