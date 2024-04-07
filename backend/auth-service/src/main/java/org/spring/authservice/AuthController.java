344f8b6e1638f64f584c4e909e87e02aefdb0fb4
Aleksandr Seppenen <alex.seppenen@gmail.com>
2024-04-07 22:38:13 +0300

    Removing AuthFacade.java

--- backend/auth-service/src/main/java/org/spring/authservice/AuthController.java
+++ backend/auth-service/src/main/java/org/spring/authservice/AuthController.java
@@ -1,35 +1,46 @@
 package org.spring.authservice;
 
+import org.spring.authservice.service.JwtService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
 public class AuthController {
-    private final AuthFacade authFacade;
-    public AuthController(AuthFacade authFacade) {
-        this.authFacade = authFacade;
+    private static final String STATUS = "status";
+    private static final String TOKEN_VALID = "tokenValid";
+    private static final String TOKEN_KEY = "token";
+    private final JwtService jwtService;
+    public AuthController(JwtService jwtService) {
+        this.jwtService = jwtService;
     }
 
     @PostMapping("/auth")
     public Mono<Map<String, String>> validateToken() {
-        return authFacade.validateToken();
+        return Mono.just(Map.of(
+                STATUS, "success",
+                TOKEN_VALID, "true")
+        );
     }
 
     @GetMapping("/token")
     //TODO: forbid this endpoint in production
-    public Mono<Map<String, Object>> generateAuthenticationToken() {
-        return authFacade.createNewToken();
+    public Mono<Map<String, String>> generateAuthenticationToken() {
+        return Mono.just(Collections.singletonMap(
+                TOKEN_KEY,
+                jwtService.generateToken()
+        ));
     }
 
     @GetMapping("/health")
     public Mono<Map<String, Object>> getServiceHealthStatus() {
-        return authFacade.checkServiceHealth();
+        return Mono.just(Collections.singletonMap(STATUS, "UP"));
     }
 }
 
