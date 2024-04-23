03066c42f84c18a63d16f67794adaa4813002359
Aleksandr Seppenen <alex.seppenen@gmail.com>
2024-04-23 16:36:02 +0300

    frontend init

---
+++ backend/umbrella-service/src/main/java/org/umbrella/service/JwtService.java
@@ -0,0 +1,48 @@
+package org.umbrella.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Service
+public class JwtService {
+    public static final String SECRET = "357638792F423F4428472B4B6250655368566D597133743677397A2443264629";
+
+
+    private String createToken(Map<String, Object> claims) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 15))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    public String generateAccessToken() {
+        //TODO: Method used for testing purposes only
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("iss", "umbrella-service");
+        return createToken(claims);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
