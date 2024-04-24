ae483468fd4e92d81a84b5a4c8345512c0d32e63
Aleksandr Seppenen <alex.seppenen@gmail.com>
2024-04-24 14:28:39 +0300

    Landing page, login & registration form UI

--- frontend/front_umbrella/src/App.tsx
+++ frontend/front_umbrella/src/App.tsx
@@ -8,16 +8,10 @@ function App() {
   return (
       <>
           <Header />
-
-
           <div className="flex flex-col items-center justify-center min-h-screen text-center py-20 bg-white-500">
               <h1 className="text-6xl font-bold text-gray-700 mb-4">Hello, Umbrella!</h1>
               <p className="text-2xl text-gray-700 mb-4">Finances should be simple.</p>
-              <a
-                  className="font-custom py-3 px-10 bg-white hover:bg-gray-200 text-blue-500 font-semibold rounded-lg"
-                  href="#"
-                  rel="noopener noreferrer"
-              >
+              <a className="font-custom py-3 px-10 bg-white hover:bg-gray-200 text-blue-500 font-semibold rounded-lg" href="#" rel="noopener noreferrer">
                   Join us
               </a>
           </div>
