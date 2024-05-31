e030f2e2a3c21d5a3cd261d3fb4beb604491b2c6
Aleksandr Seppenen <alex.seppenen@gmail.com>
2024-05-31 16:59:30 +0300

    Building response in headers

--- backend/auth-service/src/main/java/org/spring/authservice/AuthController.java
+++ backend/auth-service/src/main/java/org/spring/authservice/AuthController.java
@@ -6,14 +6,12 @@ import org.spring.authservice.enums.TokenEnum;
 import org.spring.authservice.service.JwtService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
 import java.util.Map;
 
 import static org.spring.authservice.utility.TokenUtility.ACCESS_TOKEN;
@@ -53,10 +51,7 @@ public class AuthController extends BaseController {
                         .build());
     }
 
-    @GetMapping("/health")
-    public Mono<Map<String, String>> getServiceHealthStatus() {
-        return Mono.just(Collections.singletonMap(STATUS, "UP"));
-    }
+
 }
 
 
--- backend/auth-service/src/main/java/org/spring/authservice/BaseController.java
+++ backend/auth-service/src/main/java/org/spring/authservice/BaseController.java
@@ -3,12 +3,18 @@ package org.spring.authservice;
 import org.spring.authservice.entity.TokenResponseEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.Map;
 
 import static org.spring.authservice.utility.TokenUtility.ACCESS_TOKEN;
 import static org.spring.authservice.utility.TokenUtility.REFRESH_TOKEN;
 
 public abstract class BaseController {
+
+    private static final String STATUS = "status";
 
     protected Mono<ResponseEntity<Void>> buildResponseWithHeaders(TokenResponseEntity tokenResponseEntity) {
         HttpHeaders headers = new HttpHeaders();
@@ -17,4 +23,8 @@ public abstract class BaseController {
         return Mono.just(ResponseEntity.ok().headers(headers).build());
     }
 
+    @GetMapping("/health")
+    public Mono<Map<String, String>> getServiceHealthStatus() {
+        return Mono.just(Collections.singletonMap(STATUS, "UP"));
+    }
 }
