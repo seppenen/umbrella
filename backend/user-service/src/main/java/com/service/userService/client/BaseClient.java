8895ef40ca27624784b8eb84986e9b4e933cbc6d
Aleksandr Seppenen <alex.seppenen@gmail.com>
2024-06-21 12:23:07 +0300

    deleted sendErrorResponse

--- backend/user-service/src/main/java/com/service/userService/client/BaseClient.java
+++ backend/user-service/src/main/java/com/service/userService/client/BaseClient.java
@@ -1,15 +1,9 @@
 package com.service.userService.client;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
-
-import java.util.Map;
 
 public abstract class BaseClient {
 
@@ -25,19 +19,6 @@ public abstract class BaseClient {
                 .build();
     }
 
-    public ResponseStatusException sendErrorResponse(WebClientResponseException e) {
-        try {
-            String respBody = e.getResponseBodyAsString();
-            if (!respBody.isBlank()) {
-                var errorResponse = new ObjectMapper().readValue(respBody, Map.class);
-                String errorMessage = String.valueOf(errorResponse.get("detail"));
-                return new ResponseStatusException(e.getStatusCode(), errorMessage);
-            }
-        } catch (Exception ex) {
-            throw new HttpMessageNotReadableException("Cannot parse JSON response body");
-        }
-        return null;
-    }
     protected Mono<Boolean> isResponseStatus2xxSuccessful(ClientResponse response) {
         return Mono.just(response.statusCode().is2xxSuccessful());
     }
